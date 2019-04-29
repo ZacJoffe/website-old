32c07a8b4891d7f3c45373ea03acc5a80914ae0d
ZacJoffe <zacharyjoffe@gmail.com>
2019-04-29 19:49:49 -0400

    Added router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
-import '@mdi/font/css/materialdesignicons.css' 
+import '@mdi/font/css/materialdesignicons.css'
 import './plugins/vuetify'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
