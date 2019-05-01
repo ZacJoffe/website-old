b5c656ed99b834f3327888c348980c4dfad9199d
ZacJoffe <zacharyjoffe@gmail.com>
2019-04-30 21:54:11 -0400

    Added button to download resume

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -4,7 +4,8 @@
       <v-layout row wrap>
         <v-flex justify-center>
           <v-card>
-            <pdf :src="link"></pdf>
+            <pdf :src="resumeLink"></pdf>
+            <v-btn @click="downloadResume">Download</v-btn>
           </v-card>
         </v-flex>
       </v-layout>
@@ -14,6 +15,7 @@
 
 <script>
 import pdf from 'vue-pdf'
+import axios from 'axios'
 
 export default {
   components: {
@@ -22,7 +24,23 @@ export default {
   data() {
     return {
       //link: "https://docs.google.com/viewer?url=raw.github.com/ZacJoffe/latex-resume/master/resume/zac_joffe_resume.pdf"
-      link: "https://cors-anywhere.herokuapp.com/https://nbviewer.jupyter.org/github/ZacJoffe/latex-resume/blob/master/resume/zac_joffe_resume.pdf" // fix for prod
+      resumeLink: "https://cors-anywhere.herokuapp.com/https://nbviewer.jupyter.org/github/ZacJoffe/latex-resume/blob/master/resume/zac_joffe_resume.pdf" // fix for prod
+    }
+  },
+  methods: {
+    downloadResume() {
+      axios({
+        url: this.resumeLink,
+        method: 'GET',
+        responseType: 'blob',
+      }).then((response) => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'zac-joffe-resume.pdf');
+        document.body.appendChild(link);
+        link.click();
+      })
     }
   }
 }
