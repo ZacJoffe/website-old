32c85b7b6fe2c09f5efaa764fb16bfa419413e1a
ZacJoffe <zacharyjoffe@gmail.com>
2019-05-01 22:33:08 -0400

    Removed inconsistency in imports

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import About from './views/About.vue'
 import Projects from './views/Projects.vue'
 import Resume from './views/Resume.vue'
 
@@ -18,10 +19,7 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: About
     },
     {
       path: '/projects',
