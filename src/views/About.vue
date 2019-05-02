0f9a2beb2337c980e9b62d576fa008406f0b99a0
ZacJoffe <zacharyjoffe@gmail.com>
2019-05-01 22:25:34 -0400

    First version of about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <v-container grid-list-md>
+      <v-layout row wrap>
+        <v-flex justify-center>
+          <v-card>
+            <v-card-title primary-title class="headline">About Me</v-card-title>
+            <v-card-text>
+              I'm a second year Software Engineering student at the University of Waterloo, class of 2023. I am super passionate about programming, computing, and software development.
+            </v-card-text>
+            <v-card-text>
+              Ever since I was a kid I was always invested in technology. It started off with the Nintendo Gamecube, where I'd play Super Monkey Ball 2 for hours with my brother trying to finish the challenge mode.
+            </v-card-text>
+            <v-card-text>
+              As I got older, I continued getting into video games which eventually lead me to building my first computer. Ever since then, I've been continually researching about hardware and love keeping up to date about new technological advancements.
+            </v-card-text>
+            <v-card-text>
+              Ever since I've started using Linux in middle school, I've been fascinated with programming and have been doing it ever since then. 
+            </v-card-text>
+            <v-card-text>
+              Outside of work, I code as a hobby. Usually I work on stuff that I find useful for my own workflow, such as automation scripts or CLI apps. I love to contribute to open source projects too, specifically those with Linux in mind.
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
