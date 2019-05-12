bf3b5ba59e74014805b47e686f670291629eaef8
ZacJoffe <zacharyjoffe@gmail.com>
2019-05-11 21:10:22 -0400

    Layout for experience page

--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -1,5 +1,54 @@
 <template>
   <div class="experience">
+    <v-container grid=list-md>
+      <v-layout row wrap>
+        <v-flex justify-center>
+          <v-card>
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">Emailtopia</div>
+                <span class="grey--text">Jan 2019 - Present</span>
+              </div>
+            </v-card-title>
+            <v-card-text>
+              test
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+
+      <v-layout row wrap>
+        <v-flex justify-center>
+          <v-card>
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">Segmentech Inc.</div>
+                <span class="grey--text">June 2018 - Aug 2018</span>
+              </div>
+            </v-card-title>
+            <v-card-text>
+              test
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+
+      <v-layout row wrap>
+        <v-flex justify-center>
+          <v-card>
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">Target Recycling</div>
+                <span class="grey--text">June 2016 - Sept 2016</span>
+              </div>
+            </v-card-title>
+            <v-card-text>
+              test
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
